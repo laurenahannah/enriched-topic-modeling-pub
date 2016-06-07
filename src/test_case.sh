046c5921dcf900d6c3e49c33321aedb232c1e62b
hooshmandshr <shokri.hooshmand@gmail.com>
2016-06-07 16:34:40 -0400

    Update test_case.sh

--- src/test_case.sh
+++ src/test_case.sh
@@ -1,15 +1,17 @@
-ENVIRONMENT=~/enriched-topic-modeling-pub/src
+# Path to enriched-topic-modeling-pub home
+OMNIMIXTURE_HOME=~
 # runs
-TEXT_INDIR=../data/Rueters/gics15/$COMP
-OMNIGRAPH_OUTDIR=../data/gics15_graph/$COMP
-MALLET_OUTDIR=../data/gics15_mallet/$COMP
-BASE_STOPWORD_FILE=./omnimixture/stopwordlist.txt
-LABEL_FILE=../data/label_SP500_01012006_11302013.csv
+TEXT_INDIR=${OMNIMIXTURE_HOME}/data/Reuters/gics15
+OMNIGRAPH_OUTDIR=${OMNIMIXTURE_HOME}/data/gics15_graph
+MALLET_OUTDIR=${OMNIMIXTURE_HOME}/data/gics15_mallet
+BASE_STOPWORD_FILE=${OMNIMIXTURE_HOME}/omnimixture/stopwordlist.txt
+LABEL_FILE=${OMNIMIXTURE_HOME}/data/label_SP500_01012006_11302013.csv
 
 cd ${TEXT_INDIR}
+ls
 for COMP in *; do
-
-    cd ${ENVIRONMENT}/omnigraph
+    echo "PROCESSING "$COMP
+    cd ${OMNIMIXTURE_HOME}/pub/omnigraph
     sh process.sh seg ${TEXT_INDIR}/$COMP ${OMNIGRAPH_OUTDIR}/$COMP
     sh process.sh tok ${TEXT_INDIR}/$COMP ${OMNIGRAPH_OUTDIR}/$COMP
     sh process.sh dep ${TEXT_INDIR}/$COMP ${OMNIGRAPH_OUTDIR}/$COMP
@@ -17,9 +19,9 @@ for COMP in *; do
     sh process.sh omn ${TEXT_INDIR}/$COMP ${OMNIGRAPH_OUTDIR}/$COMP $COMP
 
 
-    cd ${ENVIRONMENT}/omnimixture
-    sh omnimixture.sh ${OMNIGRAPH_OUTDIR} $COMP ${MALLET_OUTDIR} ${BASE_STOPWORD_FILE}
+    cd ${OMNIMIXTURE_HOME}/pub/omnimixture
+    sh omnimixture.sh ${OMNIGRAPH_OUTDIR} $COMP ${MALLET_OUTDIR}/$COMP ${BASE_STOPWORD_FILE}
 done
 
-cd ${ENVIRONMENT}/prediction
+cd ${OMNIMIXTURE_HOME}/prediction
 sh prediction.sh ${MALLET_OUTDIR} ${LABEL_FILE}
